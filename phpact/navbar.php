1d1b68fb6cc16ca6accdcc204efbbbec5c546ace
angelica-ctrl <angelpitaramirez1@gmail.com>
2025-10-31 12:03:18 +0800

    Create navbar.php

---
+++ phpact/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="#">
+      <i class="bi bi-basket2-fill"></i> FastFood Express
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto d-flex flex-row gap-3">
+        <li class="nav-item"><a class="nav-link" href="index.php" title="Home"><i class="bi bi-house-door fs-4"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="about.php" title="About"><i class="bi bi-info-circle fs-4"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="menu.php" title="Menu"><i class="bi bi-egg-fried fs-4"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="promos.php" title="Promos"><i class="bi bi-gift fs-4"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="contact.php" title="Contact"><i class="bi bi-envelope fs-4"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
