45641e04a3779082eb83b790543afbba02b187f0
angelica-ctrl <angelpitaramirez1@gmail.com>
2025-10-31 12:04:22 +0800

    Create footer.php

---
+++ phpact/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark text-light text-center p-3 mt-5">
+  <p class="mb-0">&copy; 2025 <i class="bi bi-basket2"></i> FastFood Express | All Rights Reserved</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
